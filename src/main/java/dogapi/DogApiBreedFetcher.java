19ba756472d0f91b504f4e121858e50ee9482ab0
Poggers69-PNG <72159679+Poggers69-PNG@users.noreply.github.com>
2025-10-18 16:55:34 -0400

    one more...

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -2,6 +2,7 @@ package dogapi;
 import dogapi.BreedNotFoundException;
 
 import okhttp3.OkHttpClient;
+import okhttp3.HttpUrl;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
@@ -37,12 +38,13 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
         HttpUrl url = HttpUrl.parse(baseUrl);
                 .newBuilder()
+                .addPathSegment("breed")
                 .addPathSegment(breed)
                 .addPathSegment("list")
                 .build();
 
 
-        Request request = new Request.builder()
+        Request request = new Request.Builder()
                 .url(url)
                 .build();
 
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -28,11 +28,10 @@ public class Main {
         // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
         try {
-            List subBreeds = breedFetcher.getSubBreeds(breed);
+            int subBreeds = breedFetcher.getSubBreeds(breed);
             return subBreeds.size();
         } catch (BreedNotFoundException e) {
             return 0;
         }
-        return subBreeds;
     }
 }
